a9b3ccc01b14af583d585a873d44636b155a6ca2
Yurii Yevtushenko <gositer2003@gmail.com>
2026-04-05 12:38:46 +0300

    feat(seo): add sitemap generator

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,24 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://calculator.k5sha.xyz', 
+  integrations: [
+    sitemap({
+      i18n: {
+        defaultLocale: 'uk', 
+        locales: {
+          uk: 'uk-UA',
+          en: 'en-US',
+        },
+      },
+      serialize(item) {
+        if (/codes/.test(item.url)) {
+          item.priority = 0.8;
+        }
+        return item;
+      },
+    }),
+  ],
+});
